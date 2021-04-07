f58adff229e8bdceb9431bb8e8e29b7168b0c220
Marek Dzwonnik <33802916+MarekDzw@users.noreply.github.com>
2021-04-07 13:47:30 +0200

    added gtag

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import store from "@/store";
 import vuetify from "@/plugins/vuetify";
+import VueAnalytics from "vue-analytics";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueAnalytics, {
+  id: "G-C9YJ711Q0C",
+});
 new Vue({
   store,
   vuetify,
