1ac7b3e3ad3c1666fe556989f703f0eb22fccadc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-07 13:28:32 +0000

    Deploy MarekDzw/IFCalculator to github.com/MarekDzw/IFCalculator.git:gh-pages

---
+++ precache-manifest.42b971d5bbc8875a82ec4adaa26febb2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2547d9718548230a2c",
+    "url": "/IFCalculator/css/app.9a0b3fac.css"
+  },
+  {
+    "revision": "7bf74b938e7e29682a69",
+    "url": "/IFCalculator/css/chunk-vendors.ce3c3798.css"
+  },
+  {
+    "revision": "63f9b68c46c9f150dafa2d5c54ed862c",
+    "url": "/IFCalculator/index.html"
+  },
+  {
+    "revision": "6a2547d9718548230a2c",
+    "url": "/IFCalculator/js/app.db8c8610.js"
+  },
+  {
+    "revision": "b0be30e87cd7c9597937",
+    "url": "/IFCalculator/js/chunk-2d216214.7163b552.js"
+  },
+  {
+    "revision": "ac506c3460aa92a2c5b5",
+    "url": "/IFCalculator/js/chunk-2d216257.256ee085.js"
+  },
+  {
+    "revision": "c9afa8cb1807defc18d4",
+    "url": "/IFCalculator/js/chunk-3ee637d4.0af8d3f1.js"
+  },
+  {
+    "revision": "7bf74b938e7e29682a69",
+    "url": "/IFCalculator/js/chunk-vendors.27fd9531.js"
+  },
+  {
+    "revision": "83fbc37a9970e78bf5230aeab02b0550",
+    "url": "/IFCalculator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/IFCalculator/robots.txt"
+  }
+]);
